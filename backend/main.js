c636247d541138f98bc261f5fea4c93bbdce4765
neistoh <hosteinluc@gmail.com>
2024-01-15 15:52:36 +0100

    commit mongodb #1

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,16 @@
-console.log('Hello world');
+const { MongoClient } = require('mongodb');
+
+// Remplacez <connection_string> par votre propre chaîne de connexion
+const uri = 'mongodb://localhost:27017/databaseTest';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log('Connected to the database');
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
+}
+
+connectToDatabase();
