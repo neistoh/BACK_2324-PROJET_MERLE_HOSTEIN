bdbaa1fe9b84067bf1ca64d4090cb1e3eb652dec
Hugo MERLE <hmerle14@gmail.com>
2024-02-23 14:59:17 +0100

    Basic app

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,19 @@
-console.log('Hello world');
+const express = require('express');
+const path = require("path");
+const http = require('http');
+const {Server} = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const port = 3000;
+
+app.use('/', express.static(path.join(__dirname, "client"), {index: "index.html"}));
+
+io.on('connection', (socket) => {
+    console.log(`New connection. Socket id : ${socket.id}`);
+});
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
