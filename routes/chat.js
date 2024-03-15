3fcdc24e6c4414fdc759424a77749dbfb8005eb9
Hugo MERLE <hmerle14@gmail.com>
2024-03-15 09:58:16 +0100

    Added MongoDB connexions

--- routes/chat.js
+++ routes/chat.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
 //TODO : Logique
 // /chat : Récupérer la liste des conversations et les afficher
 // si clic sur la conversation XXXXXXXX, rediriger vers /chat/XXXXXXXX ?
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
 
 //TODO : Logique
 // /user : Lire les données en BDD et les afficher
