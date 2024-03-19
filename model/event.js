33e4e5d86ac950e6ae389d5923adae46f4ad0b20
Hugo MERLE <hmerle14@gmail.com>
2024-03-19 10:13:44 +0100

    Added Event Filter

--- model/event.js
+++ model/event.js
@@ -1,45 +1,75 @@
 const {ObjectId} = require("mongodb");
 const Event = {
+    /**
+     * Get all events in the collection "events"
+     * @param dbName
+     * @param client
+     * @returns {*}
+     */
     getAllEvents: function (dbName, client) {
         const today = new Date();
         const db = client.db(dbName);
         return db.collection("events").find({"date": {$gte: today}}).toArray();
     },
 
+    /**
+     * Get an event by its unique ID
+     * @param dbName
+     * @param client
+     * @param id
+     * @returns {*}
+     */
     getEvent: function (dbName, client, id) {
         const db = client.db(dbName);
         return db.collection("events").find({"_id": id}).toArray();
     },
 
+    /**
+     * Add an event in the collection "events"
+     * @param dbName
+     * @param client
+     * @param event
+     * @returns {*}
+     */
     insertEvent: function (dbName, client, event) {
         const db = client.db(dbName);
         return db.collection("events").insertOne(event);
     },
 
+    /**
+     * Update the data of an event based on its ID
+     * @param dbName
+     * @param client
+     * @param id
+     * @param event
+     * @returns {*}
+     */
     updateEvent: function (dbName, client, id, event) {
         const db = client.db(dbName);
         const query = {_id: new ObjectId(id)};
         return db.collection("users").updateOne(query, event);
     },
 
-    getEventByName: function (dbName, client, name) {
+    /**
+     * Get a list of events filtered by a combination of name, theme and price (greater or equal than and lesser or equal than)
+     * @param dbName
+     * @param client
+     * @param name
+     * @param theme
+     * @param price
+     * @returns {*}
+     */
+    getEventsFiltered: function (dbName, client, name, theme, price) {
         const db = client.db(dbName);
-        return db.collection("events").find({"name": name}).toArray();
-    },
+        let query = {}
 
-    getEventByPriceGTE: function (dbName, client, price) {
-        const db = client.db(dbName);
-        return db.collection("events").find({"price": {$gte: price}}).toArray();
-    },
+        if (name) query.name = name;
+        if (theme) query.theme = theme;
+        if (price) {
+            query.price = price >= 0 ? {$gte: price} : {$lte: Math.abs(price)};
+        }
 
-    getEventByPriceLTE: function (dbName, client, price) {
-        const db = client.db(dbName);
-        return db.collection("events").find({"price": {$lte: price}}).toArray();
-    },
-
-    getEventByTheme: function (dbName, client, theme) {
-        const db = client.db(dbName);
-        return db.collection("events").find({"theme": theme}).toArray();
+        return db.collection("events").find({"name": name, "theme": theme, "price": {$lte: price}})
     }
 }
 
--- routes/eventRouter.js
+++ routes/eventRouter.js
@@ -4,7 +4,7 @@ const dbManager = require('../MongoDB/dbManager')
 const event = require('../model/event')
 
 /**
- * Récupère les messages d'une conversation d'un utilisateur
+ * Récupère les events d'un utilisateur
  */
 router.get('/:id', async (req, res) => {
     let eventData = await event.getEvent(dbManager.getDBname(), dbManager.getClient(), req.params.id)
@@ -33,6 +33,14 @@ router.put('/:id', async (req, res) => {
     } catch (err) {
         res.status(400).send({error: err.message})
     }
+})
+
+/**
+ * Filtre les events selon les paramètres de l'URL
+ */
+router.get('/:filter', async (req, res) => {
+    let eventData = await event.getEventsFiltered(dbManager.getDBname(), dbManager.getClient(), req.query.name, req.query.theme, req.query.price);
+    res.json({eventsData: eventData})
 })
 
 module.exports = router;
