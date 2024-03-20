d0046316fce6493bb181c1401f03f99a880d799c
Hugo MERLE <hmerle14@gmail.com>
2024-03-20 16:15:51 +0100

    Added socket chat support

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const chatRouter = require(path.join(__dirname, "/routes/chatRouter"));
 const eventRouter = require(path.join(__dirname, "/routes/eventRouter"));
 const userRouter = require(path.join(__dirname, "/routes/userRouter"));
 
+const dbManager = require('./MongoDB/dbManager');
+const chat = require("./model/chat")
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
@@ -20,6 +23,40 @@ app.use(express.json());
 
 io.on('connection', (socket) => {
     console.log(`New connection. Socket id : ${socket.id}`);
+    /**
+     * If a user disconnect
+     */
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    /**
+     * Return all chats of a user
+     */
+    socket.on('getAllChats', (nickname) => {
+        io.emit(chat.getAllChats(dbManager.getDBname(), dbManager.getClient(), nickname))
+    });
+
+    /**
+     * Return the messages of a chat
+     */
+    socket.on('getChat', (id) => {
+        io.emit(chat.getChat(dbManager.getDBname(), dbManager.getClient(), id));
+    });
+
+    /**
+     * Create a chat
+     */
+    socket.on('createChat', (chat) => {
+        io.emit(chat.createChat(dbManager.getDBname(), dbManager.getClient(), chat));
+    });
+
+    /**
+     * Add a message in a chat
+     */
+    socket.on("postMessage", (msg) => {
+        io.emit(chat.insertMessage(dbManager.getDBname(), dbManager.getClient(), msg))
+    })
 });
 
 app.use("/chat", chatRouter);
