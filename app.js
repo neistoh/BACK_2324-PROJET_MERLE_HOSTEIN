bde32a89f87c2cb2582d3e54ab65c322988a4a5b
neistoh <hosteinluc@gmail.com>
2024-03-22 09:22:21 +0100

    Favoris de tous les users

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const server = http.createServer(app);
 const io = require('socket.io')(server, {
     cors: {
         origin: '*',
+        methods: ["GET", "POST"],
+        credentials: true
     }
 });
 
